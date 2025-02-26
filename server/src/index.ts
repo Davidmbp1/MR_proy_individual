46b81fbd93a47b656b4437ad5bbdbfcc3e963bdf
Davidbp <marlon.bustos.p@uni.pe>
2025-02-25 21:01:15 -0500

    ok

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,6 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import path from 'path';
-
-import logger from './config/logger';
 import { connectDB } from './config/database';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
@@ -15,6 +13,7 @@ import webhookRoutes from './routes/webhook.routes';
 import purchaseRoutes from './routes/purchase.routes';
 import reviewRoutes from './routes/review.routes';
 import emailRoutes from './routes/email.routes';
+import logger from './config/logger';
 
 dotenv.config();
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
@@ -36,15 +35,16 @@ export async function createApp() {
     })
   );
 
-  // Para webhook con raw body
+  // Rutas para webhook (raw body)
   app.use('/webhook', express.raw({ type: 'application/json' }));
   app.use('/webhook', webhookRoutes);
 
   app.use(express.json());
 
+  // Sirve archivos estáticos (por ejemplo, para las imágenes subidas)
   app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
 
-  // Rutas
+  // Monta las rutas de la API
   app.use('/api/auth', authRoutes);
   app.use('/api/users', userRoutes);
   app.use('/api/restaurants', restaurantRoutes);
